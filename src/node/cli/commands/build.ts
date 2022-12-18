9a68e2644f43ff665ffe3ac3de77318230bc933f
plasticine9750 <975036719@qq.com>
2022-12-18 14:28:53 +0800

    refactor(cli): rename command action

--- src/node/cli/commands/build.ts
+++ src/node/cli/commands/build.ts
@@ -1,11 +1,13 @@
 import { CAC } from 'cac'
 
 function registerBuild(cli: CAC) {
-  cli
-    .command('build <root>', 'Build for production.')
-    .action((root: string) => {
-      console.log(`build: ${root}`)
-    })
+  /**
+   * @description build 命令入口
+   * @param root vite root -- 项目根目录（index.html 文件所在的位置）。可以是一个绝对路径，或者一个相对于该配置文件本身的相对路径
+   */
+  const actionCallback = () => {}
+
+  cli.command('build <root>', 'Build for production.').action(actionCallback)
 }
 
 export { registerBuild }
--- src/node/cli/commands/dev.ts
+++ src/node/cli/commands/dev.ts
@@ -3,13 +3,17 @@ import { CAC } from 'cac'
 import { createDevServer } from '../../core'
 
 function registerDev(cli: CAC) {
-  const action = async (root: string) => {
+  /**
+   * @description dev 命令入口
+   * @param root vite root -- 项目根目录（index.html 文件所在的位置）。可以是一个绝对路径，或者一个相对于该配置文件本身的相对路径
+   */
+  const actionCallback = async (root: string) => {
     const server = await createDevServer(root)
     await server.listen()
     server.printUrls()
   }
 
-  cli.command('dev <root>', 'Start a dev server.').action(action)
+  cli.command('dev <root>', 'Start a dev server.').action(actionCallback)
 }
 
 export { registerDev }
